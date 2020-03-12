053817d134baf8fa89d04181f356f496de46980c
erolcalisgan <erolcalisgan@hotmail.com>
2020-03-12 23:03:51 +0300

    added Arrays in Objects

--- AJavascript Array of Objects.js
+++ AJavascript Array of Objects.js
@@ -27,3 +27,37 @@ displayPoint(points);
 points.shift();
 console.log("After shift: ");
 displayPoint(points); 
+
+
+
+//Arrays in Objects
+//Aþaðýdaki örnekte, deðerleri saklayan bir nesne oluþturuyoruz.
+//from  www.java2s.com
+function MyData() { 
+    this.dataStore = [];
+    this.add = add;
+    this.average = average;
+}
+
+function add(temp) {
+    this.dataStore.push(temp);
+}
+function average() {
+    var total = 0;
+    for (var i = 0; i < this.dataStore.length; ++i) {
+        total += this.dataStore[i];
+    }
+    return total / this.dataStore.length;
+}
+
+
+var myData = new MyData();
+myData.add(2);
+myData.add(5);
+myData.add(1);
+myData.add(5);
+myData.add(5);
+myData.add(0);
+myData.add(2);
+myData.add(9);
+console.log(myData.average()); 
